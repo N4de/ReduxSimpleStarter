dd8385c2abbf9234511df1dbbafee372a5f50c21
N4de <JaAnnala@Gmail.com>
2017-08-21 11:43:14 +0300

    add logic to video_list that takes props from App

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyD8n2Tw0BZSnMHqwxwFt1PB--UIAt8M3YI';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
 		return (
 		<div>
 			<SearchBar />
+			<VideoList videos={this.state.videos}/>
 		</div>
 		);
 	}
